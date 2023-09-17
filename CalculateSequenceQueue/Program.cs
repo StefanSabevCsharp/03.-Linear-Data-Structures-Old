e4d4556da2e38fb3f19d761adeff5f701cbf5824
Stefan Sabev <stevenbg91@gmail.com>
2023-09-17 12:57:04 +0200

    Filling Queue with specified requirements

---
+++ CalculateSequenceQueue/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Queue<int> queue = new Queue<int>();
+
+    queue.Enqueue(n);
+while (queue.Count < 50)
+{
+    queue.Enqueue(n + 1);
+    queue.Enqueue(2 * n + 1);
+    queue.Enqueue(n + 2);
+    n++;
+
+}
+
+Console.WriteLine(string.Join(" ",queue));
